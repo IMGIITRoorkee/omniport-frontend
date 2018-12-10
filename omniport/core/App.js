d8e87df96224b1de114ca195dcf914af1da4476e
pradumangoyal <pgoyal@ma.iitr.ac.in>
2018-12-11 00:58:02 +0530

    Modify `App.js` to render a view only if the user has proper permissions for the view and add a temporary 404 view
    (cherry picked from commit 32be74916efbdc9337e2809352b4bf9212259338)

--- omniport/core/App.js
+++ omniport/core/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
-
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import Loadable from 'react-loadable'
+import { connect } from 'react-redux'
 
+import NoMatch from './NoMatch'
+import { setAppList } from 'core/common/src/actions/appList'
 import Loading from 'formula_one/src/components/loading'
 
 import configs from './configs.json'
@@ -11,8 +13,13 @@ import configs from './configs.json'
  This the the entry point for Omniport
 */
 
-export default class App extends React.Component {
+class App extends React.Component {
+  componentDidMount () {
+    this.props.SetAppList()
+  }
+
   render () {
+    const { appList } = this.props
     return (
       <BrowserRouter>
         <Switch>
@@ -28,20 +35,42 @@ export default class App extends React.Component {
               />
             )
           })}
-          {configs.apps.map((app, index) => {
-            return (
-              <Route
-                path={app.baseUrl}
-                key={index}
-                component={Loadable({
-                  loader: () => import(`apps/${app.source}`),
-                  loading: Loading
-                })}
-              />
-            )
-          })}
+          {appList.isLoaded &&
+            appList.data.map((app, index) => {
+              return (
+                <Route
+                  path={app.baseUrl}
+                  key={index}
+                  component={Loadable({
+                    loader: () => import(`apps/${app.source}`),
+                    loading: Loading
+                  })}
+                />
+              )
+            })}
+          {/* Temporary 404 page */}
+          {appList.isLoaded && <Route component={NoMatch} />}
         </Switch>
       </BrowserRouter>
     )
   }
 }
+
+function mapStateToProps (state) {
+  return {
+    appList: state.appList
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    SetAppList: () => {
+      dispatch(setAppList())
+    }
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
