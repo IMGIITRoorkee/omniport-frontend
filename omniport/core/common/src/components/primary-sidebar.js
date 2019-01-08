d656a6beb7e37606195899a9ba8760cac621539e
Praduman Goyal <goyal.praduman611@gmail.com>
2019-01-08 22:14:29 +0530

    Add autogeneration to primary sidebar using Discovery and add support for themes

--- omniport/core/common/src/components/primary-sidebar.js
+++ omniport/core/common/src/components/primary-sidebar.js
@@ -1,51 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import { Icon, Menu, Popup } from 'semantic-ui-react'
-import { isBrowser, isMobile } from 'react-device-detect'
+import { isMobile } from 'react-device-detect'
+
+import { getTheme } from 'formula_one'
+import config from 'core/primarySidebarConfigs.json'
 
 import '../css/primary-sidebar.css'
 
 class PrimarySidebar extends React.Component {
   state = {
-    sideBarMenuItems: [
-      {
-        icon: 'newspaper',
-        name: 'Feed',
-        path: '/'
-      },
-      {
-        icon: 'calendar alternate outline',
-        name: 'Events',
-        path: '/events'
-      },
-      {
-        icon: 'cubes',
-        name: 'Apps',
-        path: '/apps'
-      },
-      {
-        icon: 'group',
-        name: 'Groups',
-        path: '/groups'
-      },
-      {
-        icon: 'chain',
-        name: 'Links',
-        path: '/links'
-      },
-      {
-        icon: 'setting',
-        name: 'Settings',
-        path: '/settings'
-      },
-      {
-        icon: 'question',
-        name: 'Helpcentre',
-        path: '/helpcentre'
-      }
-    ]
+    sideBarMenuItems: config['services']
   }
 
   getSideBarMenuItems = () => {
@@ -67,15 +33,13 @@ class PrimarySidebar extends React.Component {
   }
 
   render () {
-    const themeColor = localStorage.getItem('themeColor') || 'blue'
-
     return (
       <Menu
         icon
         attached
         inverted
         vertical={!isMobile}
-        color={themeColor}
+        color={getTheme()}
         styleName='primary-sidebar'
       >
         {this.getSideBarMenuItems()}
