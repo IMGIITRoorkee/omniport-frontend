e1026ce79d74bc29f59ef5131fdf6f6d44bb6c80
Dhruv Bhanushali <dhruv_b@live.com>
2019-03-12 13:28:05 +0530

    Add a cascading system of clone scripts

---
+++ scripts/clone/everything.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Clone Formula 1
+bash ./scripts/clone/formula_one.sh
+
+# Clone services
+bash ./scripts/clone/services.sh
---
+++ scripts/clone/services.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Import the utility functions
+source ./scripts/clone/utils.sh
+
+# Enter the directory to clone into
+cd omniport/services/
+
+# Clone services
+clonerepo   "Apps"          "omniport-frontend-apps"            "apps"
+clonerepo   "Auth"          "omniport-frontend-auth"            "auth"
+clonerepo   "Developer"     "omniport-frontend-developer"       "developer"
+clonerepo   "Feed"          "omniport-frontend-feed"            "feed"
+clonerepo   "Groups"        "omniport-frontend-groups"          "groups"
+clonerepo   "Helpcentre"    "omniport-frontend-helpcentre"      "helpcentre"
+clonerepo   "Links"         "omniport-frontend-links"           "links"
+clonerepo   "OAuth"         "omniport-frontend-oauth"           "oauth"
+clonerepo   "Settings"      "omniport-frontend-settings"        "settings"
+clonerepo   "Terms of use"  "omniport-frontend-terms-of-use"    "terms_of_use"
