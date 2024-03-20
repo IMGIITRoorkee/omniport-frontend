d04daff5b6d64cd3980aee8ccd85ffe9b0ee4062
adityachopra29 <114332875+adityachopra29@users.noreply.github.com>
2024-03-20 22:46:23 +0530

    Update tailwind.config.js

--- omniport/tailwind.config.js
+++ omniport/tailwind.config.js
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-<<<<<<< HEAD
-    content: ["./core/**/*.{html,js,jsx}"],
-=======
     content: [
         "./core/**/*.{html,js,jsx}", 
         "./apps/**/*.{html,js,jsx}", 
         "./services/**/*.{html,js,jsx}"
     ],
->>>>>>> b26df0f6748c8ca1cfb8fdcce829a38276514f04
     theme: {},
     plugins: [],
 };
